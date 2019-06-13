7e450645babce82cb956bf17e10862c066ece0f4
Vinni Hoke <vinni.hoke@gmail.com>
2019-06-12 21:13:53 -0700

    Line 36, Tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,8 +6,6 @@ class Tab {
 		// Get the custom data attribute on the Link
 		this.tabNumber = tabElement.dataset.tab;
 		this.content = document.querySelector(`div.tabs-item[data-tab="${this.tabNumber}"]`);
-		console.log(this.content);
-		// this.content.classList.toggle(".tabs-item");
 		this.element.addEventListener("click", () => {
 
 		});
@@ -18,33 +16,30 @@ class Tab {
 
 		// Using the Item element, create a new instance of the TabItem class
 		this.item = new TabItem(this.element);
-		console.log(this.item, "This is supposed to be the tabItem");
 		// Add a click event listener on this instance, calling the select method on click
-		this.item.addEventListener("click", this.select.bind(this));
+		this.item.element.addEventListener("click", this.select.bind(this));
 	};
 
 	select() {
 		// Get all of the elements with the tabs-link class
 		const links = document.querySelector(".tabs-link");
-		console.log(links);
 
 		// Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-		// Array.from(links).forEach();
+		Array.from(links).forEach(link => {
+			this.element.classList.remove(".tabs-link-selected");
+		});
 
 		// Add a class named "tabs-link-selected" to this link
-		// this.element;
+		this.element.classList.add(".tabs-link-selected");
 
 		// Call the select method on the item associated with this link
-
+		this.element.addEventListener("click", this.select.bind(this));
 	}
 }
-
-//Is TabItem supposed to be the content of the text area?
 
 class TabItem {
 	constructor(element) {
 		// Assign this.element to the passed in element
-		// this.element;
 		this.element = element;
 	}
 
@@ -58,7 +53,7 @@ class TabItem {
 		// Remove the class "tabs-item-selected" from each element
 		this.element.classList.remove(".tabs-item-selected");
 
-		console.log(this.element);
+		
 
 		// Add a class named "tabs-item-selected" to this element
 		this.element.classList.add(".tabs-item-selected");
